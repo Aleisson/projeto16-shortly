bb53089e47e0abeb23465ba657ebde4864b5a0d8
Aleisson <caioaleisson@gmail.com>
2022-10-16 21:33:51 -0300

    fix(table_urls): remove unique

--- src/database/queries/table_users.sql
+++ src/database/queries/table_users.sql
@@ -2,7 +2,7 @@ CREATE TABLE
   users (
     id serial PRIMARY KEY,
     name character varying(80) NOT NULL,
-    email character varying(120) NOT NULL,
+    email character UNIQUE varying(120) NOT NULL,
     hash_password text NOT NULL,
     "creatAt" TIMESTAMP DEFAULT  CURRENT_TIMESTAMP
   );
